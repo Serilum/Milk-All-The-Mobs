08bd5cc23b3194052e6719d2f79080b368ea71e4
Rick South <rick@natamus.com>
2023-12-12 09:43:47 +0100

    Milk All The Mobs source code for 1.20.4.

---
+++ Common/src/main/java/com/natamus/milkallthemobs/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.milkallthemobs.util;
+
+public class Reference {
+	public static final String MOD_ID = "milkallthemobs";
+	public static final String NAME = "Milk All The Mobs";
+	public static final String VERSION = "3.1";
+	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
+}
