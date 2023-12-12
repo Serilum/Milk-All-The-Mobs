54b53ca973fd62ccbe3ead130a4af4ed290a3fa5
Rick South <rick@natamus.com>
2023-12-12 09:43:43 +0100

    Milk All The Mobs source code for 1.20.3.

---
+++ NeoForge/src/main/java/com/natamus/milkallthemobs/ModNeoForge.java
@@ -0,0 +1,32 @@
+package com.natamus.milkallthemobs;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.milkallthemobs.neoforge.events.NeoForgeMilkEvent;
+import com.natamus.milkallthemobs.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeMilkEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
