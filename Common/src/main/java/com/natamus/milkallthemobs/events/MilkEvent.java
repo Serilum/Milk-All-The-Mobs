26d0994a2479e951e40f115046b83f30dd0498fc
Rick South <rick@natamus.com>
2024-06-14 05:23:47 +0200

    Milk All The Mobs source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/milkallthemobs/events/MilkEvent.java
@@ -0,0 +1,40 @@
+package com.natamus.milkallthemobs.events;
+
+import com.natamus.collective.functions.EntityFunctions;
+
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+
+public class MilkEvent {
+	public static InteractionResult onEntityInteract(Player player, Level world, InteractionHand hand, Entity target, EntityHitResult hitResult) {
+		if (world.isClientSide) {
+			return InteractionResult.PASS;
+		}
+		
+		ItemStack itemstack = player.getItemInHand(hand);
+		if (itemstack.getItem().equals(Items.BUCKET)) {
+			if (EntityFunctions.isMilkable(target)) {
+				player.playSound(SoundEvents.COW_MILK, 1.0F, 1.0F);
+				itemstack.shrink(1);
+				
+				if (itemstack.isEmpty()) {
+					player.setItemInHand(hand, new ItemStack(Items.MILK_BUCKET));
+				}
+				else if (!player.getInventory().add(new ItemStack(Items.MILK_BUCKET))) {
+					player.drop(new ItemStack(Items.MILK_BUCKET), false);
+				}
+				
+				return InteractionResult.SUCCESS;
+			}
+		}
+		
+		return InteractionResult.PASS;
+	}
+}
