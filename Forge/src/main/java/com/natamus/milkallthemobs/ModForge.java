a8605945ae456e4684cb87bd1bb24941dae5bcfd
Rick South <rick@natamus.com>
2024-10-24 19:50:28 +0200

    Milk All The Mobs source code for 1.21.2.

---
+++ Forge/src/main/java/com/natamus/milkallthemobs/ModForge.java
@@ -0,0 +1,37 @@
+package com.natamus.milkallthemobs;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.milkallthemobs.forge.events.ForgeMilkEvent;
+import com.natamus.milkallthemobs.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeMilkEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
