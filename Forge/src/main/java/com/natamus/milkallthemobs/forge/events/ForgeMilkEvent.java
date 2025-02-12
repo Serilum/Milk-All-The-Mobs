1759bb1f8b526d07c4574a648ddd35b0773ef43c
Rick South <rick@natamus.com>
2025-02-12 23:26:11 +0100

    (v3.4) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/milkallthemobs/forge/events/ForgeMilkEvent.java
+++ Forge/src/main/java/com/natamus/milkallthemobs/forge/events/ForgeMilkEvent.java
@@ -4,12 +4,10 @@ import com.natamus.milkallthemobs.events.MilkEvent;
 import net.minecraft.world.InteractionResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeMilkEvent {
 	@SubscribeEvent
-	public void onEntityInteract(PlayerInteractEvent.EntityInteract e) {
+	public static void onEntityInteract(PlayerInteractEvent.EntityInteract e) {
 		if (MilkEvent.onEntityInteract(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.SUCCESS)) {
 			e.setCancellationResult(InteractionResult.SUCCESS);
 		}
