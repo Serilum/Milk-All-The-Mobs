fe3581392697e19074b10855321a93ee34a0a81f
Rick South <rick@natamus.com>
2026-04-02 16:05:55 +0200

    Milk All The Mobs source code for 26.1.1.

---
+++ Forge/src/main/java/com/natamus/milkallthemobs/forge/events/ForgeMilkEvent.java
@@ -0,0 +1,24 @@
+package com.natamus.milkallthemobs.forge.events;
+
+import com.natamus.milkallthemobs.events.MilkEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeMilkEvent {
+	public static void registerEventsInBus() {
+		// BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeMilkEvent.class);
+
+		PlayerInteractEvent.EntityInteractSpecific.BUS.addListener(ForgeMilkEvent::onEntityInteract);
+	}
+
+	@SubscribeEvent
+	public static void onEntityInteract(PlayerInteractEvent.EntityInteractSpecific e) {
+		if (MilkEvent.onEntityInteract(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.SUCCESS)) {
+			e.setCancellationResult(InteractionResult.SUCCESS);
+		}
+	}
+}
