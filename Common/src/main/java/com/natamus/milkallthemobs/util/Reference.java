17b218131fd5a87c617aeefd23c311d5d339ceb3
Rick South <rick@natamus.com>
2026-06-18 23:41:28 +0200

    Updated Milk All The Mobs to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/milkallthemobs/util/Reference.java
+++ Common/src/main/java/com/natamus/milkallthemobs/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "milkallthemobs";
 	public static final String NAME = "Milk All The Mobs";
 	public static final String VERSION = "3.4";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
--- Forge/src/main/java/com/natamus/milkallthemobs/ModForge.java
+++ Forge/src/main/java/com/natamus/milkallthemobs/ModForge.java
@@ -4,7 +4,6 @@ import com.natamus.collective.check.RegisterMod;
 import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.milkallthemobs.forge.events.ForgeMilkEvent;
 import com.natamus.milkallthemobs.util.Reference;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
